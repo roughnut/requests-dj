410a97e78fff3ae01ea95cfbe158eab51d9a28f0
roughnut <jeremy@writever.com.au>
2024-06-17 18:29:24 +1000

    add route and views for song requests

---
+++ public/js/request.js
@@ -0,0 +1,32 @@
+const requestHandler = async (event) => {
+    event.preventDefault();
+    const songTitle = document.getElementById("song-title").value.trim();
+    const artistName = document.getElementById("artist-name").value.trim();
+    const eventId = document.getElementById("event-id").value;
+
+    if (!songTitle || !artistName) {
+        alert("Please complete both song title and artist name fields.")
+        return;
+    }
+
+    if (songTitle && artistName) {
+        const response = await fetch("/api/requests", {
+          method: "POST",
+          body: JSON.stringify({ songTitle, artistName, eventId }),
+          headers: { "Content-Type": "application/json" },
+        });
+
+        if (response.ok) {
+            document.location.replace(`/api/requests/${eventId}`);
+        } else {
+            alert("Song request failed. Please try again later.");
+        }
+    }
+};
+
+
+
+// Get user request input from song-request.handlebars
+document
+.querySelector(".song-request-form")
+.addEventListener("submit", requestHandler);

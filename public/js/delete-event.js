459141a98a0b6d430597bf4372bdabed3919d48f
Lachlan <lachyhughes@bigpond.com>
2024-06-18 18:07:31 +0930

    Fixed routing issue with song request. Moved the delete event button into the event that only shows up for the user who created the event. added an update button but have not added function to it yet.

--- public/js/delete-event.js
+++ public/js/delete-event.js
@@ -1,28 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const deleteButtons = document.querySelectorAll(".delete-event");
+  const deleteButton = document.querySelector(".delete-event");
 
-
-deleteButtons.forEach((button) => {
-    button.addEventListener("click", async (event) => {
+  deleteButton.addEventListener("click", async (event) => {
       event.preventDefault();
       const eventId = event.target.getAttribute("delete-event-id");
 
       if (eventId) {
-        try {
-          const response = await fetch(`/api/events/${eventId}`, {
-            method: "DELETE",
-          });
+          try {
+              const response = await fetch(`/api/events/${eventId}`, {
+                  method: "DELETE",
+              });
 
-          if (response.ok) {
-            document.location.replace("/");
-          } else {
-            alert("Failed to delete event");
+              if (response.ok) {
+                  document.location.replace("/");
+              } else {
+                  alert("Failed to delete event");
+              }
+          } catch (error) {
+              console.error("Error:", error);
+              alert("An error occurred while deleting the event");
           }
-        } catch (error) {
-          console.error("Error:", error);
-          alert("An error occurred while deleting the event");
-        }
       }
-    });
   });
 });
